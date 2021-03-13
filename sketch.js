9d3d05019c31d4ec0b1dd3c70d9a81fd0890bdee
vidushijain2536 <71253509+vidushijain2536@users.noreply.github.com>
2021-03-13 11:51:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+var player;
+var score=0;
+var wormGroup;
+
+function setup() {
+createCanvas(600,600);
+player = createSprite(40,550,30,30);
+player.shapeColor="blue"; 
+wormGroup= new Group();
+}
+
+function draw() {
+background("black");  
+stroke("red")
+noFill();
+ellipse(100,350,40,30);
+player.x= mouseX;
+player.y= mouseY;
+
+if(player.x<150 && player.x>90 && player.y<380 && player.y>320 ){
+  text("NO CHEATING!!",200,200);
+  player.x=30;
+  player.y=30;
+}
+generateWorms();
+
+for(var i = 0 ; i< (wormGroup).length ;i++){
+  var temp = (wormGroup).get(i) ;
+  if (player.isTouching(temp)) {
+    score++;
+    temp.destroy();
+    temp=null;
+    }   
+  }
+
+  drawSprites();
+  stroke("yellow")
+  textSize(20);
+  text("Worms destroyed: "+score,350,50);
+}
+
+function generateWorms(){
+if(frameCount % 30===0){
+  console.log(frameCount);
+  var worm = createSprite(100,350,40,5);
+  worm.shapeColor="green"
+  worm.velocityX=random(-4,4);
+  worm.velocityY=random(-4,4);
+  wormGroup.add(worm);
+  }
+}
